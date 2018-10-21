40ca13b6bac15f4de9500f8fe328e0e2a3294501
InfiniteC0re <41500931+InfiniteC0re@users.noreply.github.com>
2018-10-21 21:07:26 +0500

    Update basic.js

--- plugins/basic.js
+++ plugins/basic.js
@@ -8,27 +8,26 @@ module.exports.run = async (bot, message, args, pluginList, commandList, message
   }else if(messageArray[1] == "embed") {
     let content = messageArray.slice(2);
     if(content!="") {
-      const embed = new Discord.RichEmbed()
+      let embed = new Discord.RichEmbed()
       .setTitle(content)
       .setAuthor(message.author.username, message.author.avatarURL)
       .setColor(0x00AE86);
-  }else{
-    const embed = new Discord.RichEmbed()
-    .setTitle("Сообщение не может быть пустым!")
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setColor(0x00AE86);
+      message.channel.send({embed});
+    }else{
+      const embed = new Discord.RichEmbed()
+      .setTitle("Сообщение не может быть пустым!")
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setColor(0x00AE86);
+      message.channel.send({embed});
+    }
+    }else{
+      let embed = new Discord.RichEmbed()
+      .setTitle(`Плагин Basic не нашел этой команды!\nВызовите 'basic help' для получения списка команд`)
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setColor(0x00AE86);
+      message.channel.send({embed});
+    }
   }
-
-    message.channel.send({embed});
-  }else{
-    const embed = new Discord.RichEmbed()
-    .setTitle(`Плагин Basic не нашел этой команды!\nВызовите 'basic help' для получения списка команд`)
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setColor(0x00AE86);
-
-    message.channel.send({embed});
-  }
-}
 
 module.exports.help = {
   name: "basic",
